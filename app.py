25dacbece152534354a5ca918516ed0b05a01fc0
mahmoud3798 <69508168+mahmoud3798@users.noreply.github.com>
2020-08-12 15:44:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    For rendering results on HTML GUI
     '''
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features) 
-   
+    prediction = model.predict(final_features)
+
     output = prediction[0]
     return render_template('index.html', prediction_text='Activity will be {}'.format(output))
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
